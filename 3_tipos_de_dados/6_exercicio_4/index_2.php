af6db589099ae5a21875bab4485d5bfbd7c7e9a4
Anthony <anthonpolak58@gmail.com>
2025-02-14 08:20:36 -0300

    tt

---
+++ 3_tipos_de_dados/6_exercicio_4/index_2.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 16.2;
+$b = 12.1;
+
+    if(is_float($a)) {//true
+    echo "esse valor é float <br>";
+    }
+
+    echo "$b <br>";
+
+    echo "15.3 <br>";
+
+    echo "7.1";
---
+++ 3_tipos_de_dados/9_exercicio_5/index_2.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo 'testando texto com aspas simples';
+    echo "<br>";
+    echo "testando texto com aspas duplas";
+    echo "<br>";
+
+    $str = "Anthony";
+
+    echo 'Meu nome é $str <br>';
+
+    echo "Meu nome é $str";
